4d9136dcaf3bd6ca47ac31208d27d390d5d010b6
dnqxz <dnqxz@outlook.com>
2022-05-11 21:13:49 +0800

    基本模板结构文件

---
+++ src/views/Demo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{ message }}</div>
+</template>
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+export default defineComponent({
+  setup() {
+    let message = ref('demo page')
+    return {
+      message
+    }
+  }
+})
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <input type="text" v-model="message">
+  </div>
+  <div>{{ message }}</div>
+</template>
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+export default defineComponent({
+  setup() {
+    let message = ref('home page')
+    return {
+      message
+    }
+  }
+})
+</script>
